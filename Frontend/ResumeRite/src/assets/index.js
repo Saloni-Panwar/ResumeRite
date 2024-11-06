5bc2348d9dfde4d4b024b799948ad8941d709093
saloni-panwar <salonipanwar232003@gmail.com>
2024-11-06 11:42:27 +0530

    delete unused folder

--- Frontend/ResumeRite/src/assets/index.js
+++
@@ -1,9 +0,0 @@
-import template1 from "./template1.png";
-import template2 from "./template2.png";
-import template3 from "./template3.png";
-import template4 from "./template4.png";
-import template5 from "./template5.png";
-import template6 from "./template6.png";
-
-
-export { template1, template2, template3, template4, template5 , template6};
